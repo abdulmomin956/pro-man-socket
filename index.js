ace1f0c78bd5fb12f881fd9d5baae2427d23c2cd
Abdul Momin <66585914+abdulmomin956@users.noreply.github.com>
2022-12-01 14:58:55 +0600

    group msg and refresh problem solved

--- index.js
+++ index.js
@@ -5,20 +5,46 @@ const io = require("socket.io")(8900, {
 });
 
 let users = [];
+let conversations = [];
 
 const addUser = (userId, socketId) => {
     !users.some((user) => user.userId === userId) &&
         users.push({ userId, socketId });
 };
 
+const activeConversation = (conversationId, socketId, userId) => {
+    // console.log(conversations, 16, conversationId, socketId);
+    conversations = conversations.filter(function (obj) {
+        return obj.userId !== userId;
+    })
+    // console.log(conversations, 19,);
+    !conversations.some((c) => c.conversationId === conversationId && c.socketId === socketId) &&
+        conversations.push({ conversationId, socketId, userId });
+    // console.log(conversations, 21);
+
+};
+// console.log(conversations, 21);
 const removeUser = (socketId) => {
     users = users.filter((user) => user.socketId !== socketId);
 };
 
+const removeConversation = (socketId) => {
+    conversations = conversations.filter((conversation) => conversation.socketId !== socketId);
+};
+
+// console.log(conversations, 24);
+
 const getUser = (userId) => {
-    // console.log(users);
+    // console.log(users, 33);
     if (userId) {
         return users.filter((user) => userId?.indexOf(user?.userId) !== -1);
+    }
+};
+
+const getConversation = (conversationId) => {
+    // console.log(conversations, 46);
+    if (conversationId) {
+        return conversations.filter((conversation) => conversationId?.indexOf(conversation?.conversationId) !== -1);
     }
 };
 
@@ -28,19 +54,27 @@ io.on("connection", (socket) => {
 
     //take userId and socketId from user
     socket.on("addUser", (userId) => {
+        // console.log('addUser socket on', userId);
         addUser(userId, socket.id);
         io.emit("getUsers", users);
     });
 
+    socket.on("activeConversation", ({ currentChatId, userId }) => {
+        // console.log(currentChatId, 64);
+        activeConversation(currentChatId, socket.id, userId)
+        io.emit('getActiveConversations', conversations)
+    })
+
     //send and get message
-    socket.on("sendMessage", ({ senderId, receiverId, text }) => {
-        // console.log(receiverId);
-        const user = getUser(receiverId);
-        // console.log(user);
-        user.map(({ socketId }) => {
+    socket.on("sendMessage", ({ senderId, text, conversationId }) => {
+        // console.log(conversationId);
+        const conversation = getConversation(conversationId);
+        // console.log(conversation, 72);
+        conversation.map(({ socketId }) => {
             io.to(socketId).emit("getMessage", {
                 senderId,
                 text,
+                conversationId
             });
         })
 
@@ -50,6 +84,7 @@ io.on("connection", (socket) => {
     socket.on("disconnect", () => {
         // console.log("a user disconnected!");
         removeUser(socket.id);
+        removeConversation(socket.id);
         io.emit("getUsers", users);
     });
 });
